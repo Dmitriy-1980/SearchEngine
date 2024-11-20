8a9637c9a81bb91c01101f8e442ed5e5f15ef7c6
Dmirtii Ivanov <karloper@mail.ru>
2024-11-20 16:51:04 +0700

    3 trouble with been

--- src/main/java/searchengine/Application.java
+++ src/main/java/searchengine/Application.java
@@ -5,18 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import searchengine.mechanics.Indexing;
 
 @SpringBootApplication
-//@EntityScan("searchengine")
 
 @ComponentScan("searchengine")
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "indexing-settings")
-//@Configuration
 public class Application {
 
 
